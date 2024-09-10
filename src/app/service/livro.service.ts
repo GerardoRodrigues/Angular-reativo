c90b71307c423df0ca9a5306f4eebba89e35d88c
Gerardo Rodrigues <gerardofilho56@gmail.com>
2024-09-10 15:47:23 -0300

    Fazendo a busca na API

---
+++ src/app/service/livro.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
+import { Item, LivroResultado } from '../model/Interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LivroService {
+
+  private readonly API = 'https://www.googleapis.com/books/v1/volumes'
+
+  constructor(private http: HttpClient) { }
+
+  buscar(dado: string): Observable<Item[]>{
+    const params = new HttpParams().append('q', dado);
+    return this.http.get<LivroResultado>(this.API, {params}).pipe(
+      map(resultado => resultado.items)
+    );
+  }
+}
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,16 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/model/Interfaces';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy{
 
-  listaLivros: [];
+  listaLivro: Livro[];
+  campoBusca = '';
+  subscription: Subscription;
+  livro: Livro;
 
-  constructor() { }
+  constructor(private service: LivroService) { }
 
+  buscar(){
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: item => this.listaLivro = this.livrosResultado(item),
+      error: erro => console.error(erro),
+    });
+  }
+
+  livrosResultado(items){
+    const livros: Livro[] = [];
+    items.forEach(items => {
+      livros.push(this.livro = {
+        title: items.volumeInfo?.title,
+        authors: items.volumeInfo?.authors,
+        publisher: items.volumeInfo?.publisher,
+        publishedDate: items.volumeInfo?.publishedDate,
+        description: items.volumeInfo?.description,
+        thumbnail: items.volumeInfo?.imageLinks?.thumbnail,
+        previewLink: items.volumeInfo?.previewLink
+      })
+    })
+    return livros;
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe();
+  }
 }
 
 
